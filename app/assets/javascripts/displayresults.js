8ba71c9573dc3857ed521e253cc31256a4499dec
Tony Goldsmith <tony.h.goldsmith@gmail.com>
2016-08-24 17:30:21 -0500

    altered display JS

--- app/assets/javascripts/displayresults.js
+++ app/assets/javascripts/displayresults.js
@@ -1,4 +1,7 @@
 function displayPlaces (places){
-  console.log(places.businesses[0].name);
+  var count = 0;
+  for (count = 0; count <10; count ++){
+    console.log(places.businesses[count].name);
+  }
 
 }
